18f0271d49995a7dd5cf8310907171fa1ca7855d
bk1071 <bk1071@students.cs.ubca>
2023-04-02 11:54:11 -0700

    extra commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Header.css';
 import logo from '../assets/MemoriCare.png';
+import profileImg from '../assets/profile.jpg'; // Import your profile image here
 
 function Header() {
     return (
@@ -10,23 +11,29 @@ function Header() {
                 <img src={logo} alt="Logo" className="logo" />
             </div>
             <div className="header">
-                <NavLink to="/" className="nav-link" activeClassName="nav-link-active">
-                    Dashboard
-                </NavLink>
-                <NavLink
-                    to="/patients"
-                    className="nav-link"
-                    activeClassName="nav-link-active"
-                >
-                    Patients
-                </NavLink>
-                <NavLink
-                    to="/help-log"
-                    className="nav-link"
-                    activeClassName="nav-link-active"
-                >
-                    Help Log
-                </NavLink>
+                <div className="nav-links">
+                    <NavLink to="/" className="nav-link" activeClassName="nav-link-active">
+                        Dashboard
+                    </NavLink>
+                    <NavLink
+                        to="/patients"
+                        className="nav-link"
+                        activeClassName="nav-link-active"
+                    >
+                        Patients
+                    </NavLink>
+                    <NavLink
+                        to="/help-log"
+                        className="nav-link"
+                        activeClassName="nav-link-active"
+                    >
+                        Help Log
+                    </NavLink>
+                </div>
+                <div className="profile-container">
+                    <img src={profileImg} alt="Profile" className="profile-image" />
+                    <span className="profile-text">Profile</span>
+                </div>
             </div>
         </>
     );
